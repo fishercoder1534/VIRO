174ee6382fa3d363aeda139fcb7d86477bce3c4b
Steve Sun <steve.j.sun@gmail.com>
2014-05-01 12:25:39 -0500

    added unmodified baseline code of version 2, please don't modify this folder, for the purpose of reference

---
+++ baseline_code_v2_do_NOT_modify/viro_sim_py_v2/topologies_workloads/topologies/small_topologies/7.no.workload.run.sh
@@ -0,0 +1,8 @@
+python ../../../veil_switch.pyc 7.adlist 7.vid localhost:5001 0 &
+python ../../../veil_switch.pyc 7.adlist 7.vid localhost:5002 0 &
+python ../../../veil_switch.pyc 7.adlist 7.vid localhost:5003 0 &
+python ../../../veil_switch.pyc 7.adlist 7.vid localhost:5004 0 &
+python ../../../veil_switch.pyc 7.adlist 7.vid localhost:5005 0 &
+python ../../../veil_switch.pyc 7.adlist 7.vid localhost:5006 0 &
+python ../../../veil_switch.pyc 7.adlist 7.vid localhost:5007 0 & 
+
