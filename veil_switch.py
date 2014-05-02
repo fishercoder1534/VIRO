d2111b505f60737c6e78e4f864ce9b0e9cc8b62f
Steve Sun <steve.j.sun@gmail.com>
2014-05-02 18:12:56 -0500

    moved the code chunk under for loop in routepacket function as Chris said

--- veil_switch.py
+++ veil_switch.py
@@ -345,24 +345,24 @@ def routepacket(packet):
 
             #Chris: I think the code marked below should be in the for block
             #Chris: -------------------------code start here-----------------------------------
-            if packettype == DATA_PKT:
-                fwdvid_str = getFwdVid(packet,L)
-                fwdvid = int(fwdvid_str, 2)
-                print myprintid,'FwdVid is:', hex(fwdvid)
-                if fwdvid == 0x89abcdef:
-                    print myprintid,'I am a source router!'
+    	        if packettype == DATA_PKT:
+                    fwdvid_str = getFwdVid(packet,L)
+                    fwdvid = int(fwdvid_str, 2)
+                    print myprintid,'FwdVid is:', hex(fwdvid)
+                    if fwdvid == 0x89abcdef:
+                        print myprintid,'I am a source router!'
 
-                    for t in routingTable[dist]:
-                        print myprintid,'Getting gw for level', dist
-                        gw = bin2str(t[1],L)
-                        if gw == '':
-                            print myprintid,'No gw known for this packet!'
-                        else:
-                            print myprintid,'Updating FwdVid with gw:', gw
-                            packet = updateFwdVid(packet, int(gw,2))
-                            fwdvid_str = gw
-                            fwdvid = int(fwdvid_str, 2)
-                        break
+                        for t in routingTable[dist]:
+                            print myprintid,'Getting gw for level', dist
+                            gw = bin2str(t[1],L)
+                            if gw == '':
+                                print myprintid,'No gw known for this packet!'
+                            else:
+                                print myprintid,'Updating FwdVid with gw:', gw
+                                packet = updateFwdVid(packet, int(gw,2))
+                                fwdvid_str = gw
+                                fwdvid = int(fwdvid_str, 2)
+                            break
 
                     #RJZ: Still investigating why this is happening...
                     if fwdvid == 0x89abcdef:
@@ -383,17 +383,17 @@ def routepacket(packet):
                 #     though. If we're in the "up the tree" phase, we choose the
                 #     nexthop based off the FwdVid. If we're in the "down the 
                 #     tree" phase, then we choose based on the dst.
-                if int(myvid,2) == fwdvid:
-                    packet = updateFwdVid(packet, int(dst,2))
-                    fwdvid_str = dst
-                    fwdvid = int(fwdvid_str, 2)
+                    if int(myvid,2) == fwdvid:
+                        packet = updateFwdVid(packet, int(dst,2))
+                        fwdvid_str = dst
+                        fwdvid = int(fwdvid_str, 2)
 
-                if int(dst,2) != fwdvid: # up the tree
-                    print myprintid,'Going up the tree'
-                    nexthop = getNextHop(fwdvid_str)
-                else: #down the tree
-                    print myprintid,'Going down the tree'
-                    nexthop = getNextHop(dst)
+                    if int(dst,2) != fwdvid: # up the tree
+                        print myprintid,'Going up the tree'
+                        nexthop = getNextHop(fwdvid_str)
+                    else: #down the tree
+                        print myprintid,'Going down the tree'
+                        nexthop = getNextHop(dst)
 
     # TODO: After we find the nexthop, we test to see if that node is functional
     #       *Use createEchoRequestPacket for this*
@@ -405,15 +405,15 @@ def routepacket(packet):
     #Chris: routingTable is a dictionary and then you can use the builtin function to remove items in it. e.g: del routingTable[dst]
                    # echoReply = ping(nextHop) # ping is not pre-defined and we cannot use ping any more as the TA commented
                    #using socket connect to test whether the remote host is still working
-                try:
-                    testSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                    address = nexthop.split(':')
-                    testSocket.connect(address[0], address[1])
-                    testSocket.close()
-                    print myprintid,"The next hop:", nexthop, "is up"
-                    break
-                except:
-                    print myprintid,"The next hop:", nexthop, "is down"
+                     try:
+                         testSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                         address = nexthop.split(':')
+                         testSocket.connect(address[0], address[1])
+                         testSocket.close()
+                         print myprintid,"The next hop:", nexthop, "is up"
+                         break
+                     except:
+                         print myprintid,"The next hop:", nexthop, "is down"
               #Chris:-----------------------------------------Code ends here--------------------------------------------
             
         if (packettype != RDV_PUBLISH) and (packettype != RDV_QUERY):
